6809e241cf857d0e39b5739bcb332bd37d1832a4
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-08-18 14:50:58 +0800

    + 运行实例

--- src/main/scala/Scheduler.scala
+++ src/main/scala/Scheduler.scala
@@ -1,7 +1,9 @@
 import akka.actor.{ActorSystem, MasterActor, Props}
+import akka.message.Result
 
 /**
   * Created by C.J.YOU on 2016/8/16.
+  * 程序主入口
   */
 object Scheduler {
 
@@ -9,7 +11,22 @@ object Scheduler {
 
     val system = ActorSystem("""TestAkkaSystem""")
     val masterActor = system.actorOf(Props[MasterActor], name = "MasterActor")
+
     masterActor.tell("lets do action",masterActor)
+
+    Thread.sleep(3000)  // 等待计算结束通知
+
+    masterActor ! new Result() // 告诉masterActor 我需要得到计算结果
+
+    system.terminate() // 结束actor system
+
+    /*[INFO] [08/18/2016 14:34:26.636] [TestAkkaSystem-akka.actor.default-dispatcher-5] [akka://TestAkkaSystem/user/MasterActor] master got message:lets do action
+    [INFO] [08/18/2016 14:34:26.640] [TestAkkaSystem-akka.actor.default-dispatcher-6] [akka://TestAkkaSystem/user/MasterActor/reduceActor] reduce got message:map ok!
+    [INFO] [08/18/2016 14:34:26.640] [TestAkkaSystem-akka.actor.default-dispatcher-6] [akka://TestAkkaSystem/user/MasterActor/aggregateActor] Aggregate got message:reduce ok!
+    [INFO] [08/18/2016 14:34:26.640] [TestAkkaSystem-akka.actor.default-dispatcher-6] [akka://TestAkkaSystem/user/MasterActor/aggregateActor] Aggregate ok!
+    注意: reduceActor,aggregateActor 都为 MasterActor监管，actor之间发送消息的实例需要和接受消息的实例需要一致，否则出现消息无法处理 */
+
+
   }
 
 }
